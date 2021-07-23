3dbc92d4b84ecb465612818338f8423077cfab73
Oleksandr Bretsko <bretsko.oleksandr@gmail.com>
2021-07-23 18:00:33 +0300

    Initial Commit

---
+++ SocialApp-ProgrammaticUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp-ProgrammaticUI
+//
+//  Created by Oleksandr Bretsko on 23.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
